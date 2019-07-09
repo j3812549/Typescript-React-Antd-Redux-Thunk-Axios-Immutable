cb1697505f5dececc29a66de653f2e49c1938f17
j3812549 <380012546@qq.com>
2019-07-09 17:48:35 +0800

    【A】引入less-loader，并配置好webpack

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -182,7 +182,7 @@ module.exports = {
           // use the "style" loader inside the async code so CSS from them won't be
           // in the main CSS file.
           {
-            test: /\.css$/,
+            test: /\.css|less$/,
             loader: ExtractTextPlugin.extract(
               Object.assign(
                 {
@@ -221,6 +221,9 @@ module.exports = {
                         ],
                       },
                     },
+                    {
+                      loader: require.resolve("less-loader"),
+                    }
                   ],
                 },
                 extractTextPluginOptions
--- src/components/Login/loginUI.tsx
+++ src/components/Login/loginUI.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import { ILoginProps } from './index'
-
+import { Row, Col, Input } from 'antd'
+import './index.less'
 
 class Login extends React.Component <ILoginProps> {
   constructor(props: any) {
@@ -10,7 +11,13 @@ class Login extends React.Component <ILoginProps> {
   public render () {
     const { handleLogin } = this.props
     return (
-      <div>
+      <div className="login-box">
+        <Row>
+            <Col span={6}>admin</Col>
+            <Col span={18}>
+              <Input/>
+            </Col>
+        </Row>
         <button onClick={handleLogin}>登录</button>
       </div>
     )
