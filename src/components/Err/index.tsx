b03e33bdfd1f785c8c59902ce48fd7cc09167bab
j3812549 <380012546@qq.com>
2019-07-29 22:06:24 +0800

    【D】删除Err组件中多余的测试数据

--- src/components/Err/index.tsx
+++ src/components/Err/index.tsx
@@ -1,36 +1,17 @@
 import * as React from 'react';
 import './index.less'
-import store from '../../store'
-import { connect } from 'react-redux'
 
 class Err extends React.Component<any> {
   constructor(props: any) {
     super(props)
-    console.log(store.getState())
-    this.state = {
-      aaa: '2'
-    }
   }
   public render() {
     return (
       <div className="login-box">
         错误页面
-        <input onChange={(e) => {
-          this.setState({
-            aaa: 3
-          })
-          console.log(this.state)
-        }} />
       </div>
     )
   }
 }
 
-const mapStateToProps = (state: any) => {
-  console.log(state)
-  return {
-    prop: state.prop
-  }
-}
-
-export default connect(mapStateToProps)(Err)
+export default Err
